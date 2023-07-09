3f73b91695c4989ca9e15809817d01ce95c41be8
Keith Austin <keith@keithaustin.dev>
2023-07-09 13:23:27 -0400

    More styling and add portfolio link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,21 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-8
+    <main className="relative flex min-h-screen flex-col items-center p-8
       bg-gradient-to-br from-neutral-950 from-50% to-indigo-900">
+      <a href="https://keithaustin.dev/"
+        className="absolute top-2 left-2 font-semibold underline hover:no-underline">
+          Back to Portfolio
+      </a>
       <section className="title text-2xl font-semibold text-center mb-8">
+        
         <h1>Blueberry</h1>
       </section>
       <section className="input text-center mb-4 min-w-[95%]">
         <form onSubmit={(e) => handleSubmit(e)}>
           <textarea
-            className="rounded-lg shadow-xl font-mono min-h-[300px] min-w-[340px]
-              md:min-w-[440px] lg:min-w-[640px] md:min-h-[400px] lg:min-h-[500px]
+            className="rounded-lg shadow-xl font-mono min-h-[200px] min-w-[340px]
+              md:min-w-[440px] lg:min-w-[640px] md:min-h-[260px] lg:min-h-[500px]
               bg-teal-950/30 rounded-lg shadow-xl focus:outline-none p-2"
             value={codeText}
             onChange={(e) => setCodeText(e.target.value)}
@@ -38,7 +43,7 @@ export default function Home() {
         </form>
       </section>
       <section className="output mb-4">
-        <div className="output-pane bg-teal-950/30 min-h-[144px] max-h-[280px]
+        <div className="output-pane bg-teal-950/30 min-h-[128px]
           min-w-[340px] md:min-w-[440px] lg:min-w-[640px] md:min-h-[244px] lg:min-h-[344px]
           p-4 overflow-y-auto rounded-lg flex flex-col-reverse shadow-xl font-mono">
           {
